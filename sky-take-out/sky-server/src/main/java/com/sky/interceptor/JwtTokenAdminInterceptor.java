5fb2e2403e1a4104ef835fa46b1ba2d54ba4930d
xiao <128299833+ad6414@users.noreply.github.com>
2023-08-29 16:20:04 +0800

    员工分页查询实现

--- sky-take-out/sky-server/src/main/java/com/sky/interceptor/JwtTokenAdminInterceptor.java
+++ sky-take-out/sky-server/src/main/java/com/sky/interceptor/JwtTokenAdminInterceptor.java
@@ -47,7 +47,7 @@ public class JwtTokenAdminInterceptor implements HandlerInterceptor {
             log.info("jwt校验:{}", token);
             Claims claims = JwtUtil.parseJWT(jwtProperties.getAdminSecretKey(), token);
             Long empId = Long.valueOf(claims.get(JwtClaimsConstant.EMP_ID).toString());
-            log.info("当前员工id：", empId);
+            log.info("当前员工id：{}", empId);
             //3、通过，放行
             //4、获取当前用户，设置为线程局部参数
             BaseContext.setCurrentId(empId);
